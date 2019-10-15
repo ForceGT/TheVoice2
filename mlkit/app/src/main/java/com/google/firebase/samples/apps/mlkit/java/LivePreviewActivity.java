d352f2b1584873863f9d031cee4049490a03307a
gtxtreme <thakkargaurav409@gmail.com>
2019-10-15 09:34:49 +0000

    first commit

--- mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/LivePreviewActivity.java
+++ mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/LivePreviewActivity.java
@@ -96,15 +96,15 @@ public final class LivePreviewActivity extends AppCompatActivity
 
         Spinner spinner = findViewById(R.id.spinner);
         List<String> options = new ArrayList<>();
-        options.add(FACE_CONTOUR);
-        options.add(FACE_DETECTION);
-        options.add(AUTOML_IMAGE_LABELING);
+       // options.add(FACE_CONTOUR);
+        //options.add(FACE_DETECTION);
+        //options.add(AUTOML_IMAGE_LABELING);
         options.add(OBJECT_DETECTION);
         options.add(TEXT_DETECTION);
-        options.add(BARCODE_DETECTION);
-        options.add(IMAGE_LABEL_DETECTION);
-        options.add(CLASSIFICATION_QUANT);
-        options.add(CLASSIFICATION_FLOAT);
+        //options.add(BARCODE_DETECTION);
+        //options.add(IMAGE_LABEL_DETECTION);
+        //options.add(CLASSIFICATION_QUANT);
+        //options.add(CLASSIFICATION_FLOAT);
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_style,
                 options);
